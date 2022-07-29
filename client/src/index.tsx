5e948a60651387c44a03f66683f7db89339d9019
Laza Nantenaina <laza@hairun-technology.com>
2022-07-29 22:28:09 +0300

    Setting up apollo client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { StyledEngineProvider } from "@mui/material/styles";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <ApolloProvider client={client}>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
