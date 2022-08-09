48f394f8aa5e208b5eb12d9c36d246f5ed2a1203
Laza Nantenaina <laza@hairun-technology.com>
2022-08-09 20:40:59 +0300

    Refetch after mutation

--- client/src/hooks/useCreateColumn.ts
+++ client/src/hooks/useCreateColumn.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@apollo/client";
 import { CREATE_COLUMN } from "../graphql/mutation";
+import { GET_COLUMNS } from "../graphql/query";
 import {
   CreateColumn,
   CreateColumnVariables,
@@ -9,7 +10,9 @@ export const useCreateColumn = () => {
   const [mutate, { loading, error }] = useMutation<
     CreateColumn,
     CreateColumnVariables
-  >(CREATE_COLUMN);
+  >(CREATE_COLUMN, {
+    refetchQueries: [{ query: GET_COLUMNS }],
+  });
 
   return { mutate, loading, error };
 };
