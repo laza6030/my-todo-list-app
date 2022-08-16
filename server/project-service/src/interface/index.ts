870f931f2e35551c86027e604ff2ae9af8d08949
Laza Nantenaina <laza@hairun-technology.com>
2022-08-16 20:58:45 +0300

    Some refac

--- server/project-service/src/interface/index.ts
+++ server/project-service/src/interface/index.ts
@@ -1,12 +1,10 @@
-import { ObjectId } from 'mongoose'
+import { Types } from 'mongoose'
 
 export interface IColumn {
-    id: string
     name: string
 }
 
 export interface ITask {
-    id: string
     name: string
-    columnId: ObjectId
+    columnId: Types.ObjectId
 }
