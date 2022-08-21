454e274f855c921e847342b6279d657bfeb4ef40
Laza Nantenaina <laza@hairun-technology.com>
2022-08-21 15:15:56 +0300

    Handle user not found error

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,7 @@
+export const APOLLO_ERROR_CODE = {
+  USER_NOT_FOUND: "USER_NOT_FOUND",
+};
+
 // For dnd purpose
 export const ItemTypes = {
   TASK: "task",
--- server/account-service/src/graphql/resolvers/mutation/signIn.ts
+++ server/account-service/src/graphql/resolvers/mutation/signIn.ts
@@ -2,21 +2,21 @@ import jwt from "jsonwebtoken";
 import { MutationSignInArgs } from "../../../generated/types";
 import UserModel from "../../../models/userModel";
 import { IUser } from "../../../interface";
-
 import { JWT_SECRET_KEY } from "../../../config";
+import { UserNotFoundError } from "../../../helpers/error";
 
 export const signIn = async (
   _,
   { input: { username, password } }: MutationSignInArgs
 ) => {
-  try {
-    const user: IUser = await UserModel.findOne({ username, password });
+  const user: IUser = await UserModel.findOne({ username, password });
 
-    if (user) {
-      const token = jwt.sign(username, JWT_SECRET_KEY);
-      return token;
-    }
-  } catch (error) {
-    console.error(error);
+  if (!user) {
+    throw new UserNotFoundError();
+  }
+
+  if (user) {
+    const token = jwt.sign(username, JWT_SECRET_KEY);
+    return token;
   }
 };
